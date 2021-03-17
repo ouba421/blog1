f52a936461a744d08469048bddebbc861c6482e2
ouba421 <31402685+ouba421@users.noreply.github.com>
2021-03-17 17:03:03 +0800

    添加打包静态文件到gitpages

---
+++ web/dist/js/chunk-2d20939e.9eb2b91f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20939e"],{a7c5:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},o=[],r={data:function(){return{}},beforeRouteEnter:function(n,e,t){t((function(n){n.$router.go(-1)}))}},c=r,a=t("2877"),i=Object(a["a"])(c,u,o,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d20939e.9eb2b91f.js.map
